a29fe3e9abf990aaaca81d95a651b74d79a150fd
Yoshida Shin <wbcchsyn@gmail.com>
2020-03-13 10:25:47 +0900

    Implement Default for MmapAllocator

--- src/mmap_allocator.rs
+++ src/mmap_allocator.rs
@@ -1,3 +1,19 @@
 /// Allocator whose backend is mmap(2)
 #[derive(Debug, Clone, Copy)]
 pub struct MmapAllocator;
+
+impl Default for MmapAllocator {
+    fn default() -> Self {
+        Self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default() {
+        let _alloc = MmapAllocator::default();
+    }
+}
