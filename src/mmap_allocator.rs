cb918baff1a329cb57f6a9f3f28d15141c8eee97
Yoshida Shin <wbcchsyn@gmail.com>
2020-03-13 10:38:33 +0900

    Implement GlobalAlloc for MmapAllocator
    All the methods are not implemented yet. They delegate
    std::alloc::System.

--- src/mmap_allocator.rs
+++ src/mmap_allocator.rs
@@ -1,3 +1,5 @@
+use std::alloc::{GlobalAlloc, Layout, System};
+
 /// Allocator whose backend is mmap(2)
 #[derive(Debug, Clone, Copy)]
 pub struct MmapAllocator;
@@ -8,12 +10,58 @@ impl Default for MmapAllocator {
     }
 }
 
+unsafe impl GlobalAlloc for MmapAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        System.alloc(layout)
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::mem;
+    use std::ptr;
 
     #[test]
     fn default() {
         let _alloc = MmapAllocator::default();
+    }
+
+    #[test]
+    fn allocate() {
+        unsafe {
+            type T = i64;
+            let alloc = MmapAllocator::default();
+
+            let layout = Layout::new::<i64>();
+            let ptr = alloc.alloc(layout) as *mut T;
+            assert_ne!(std::ptr::null(), ptr);
+
+            *ptr = 84;
+            assert_eq!(84, *ptr);
+
+            *ptr = *ptr * -2;
+            assert_eq!(-168, *ptr);
+
+            alloc.dealloc(ptr as *mut u8, layout)
+        }
+    }
+
+    #[test]
+    fn allocate_too_large() {
+        unsafe {
+            type T = String;
+            let alloc = MmapAllocator::default();
+
+            let align = mem::align_of::<T>();
+            let size = std::usize::MAX - mem::size_of::<T>();
+            let layout = Layout::from_size_align(size, align).unwrap();
+
+            assert_eq!(ptr::null(), alloc.alloc(layout));
+        }
     }
 }
